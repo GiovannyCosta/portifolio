6de77da4796e6aa5e80d05a5e8e61c638f026bda
Giovanny Costa <gioreizom@gmail.com>
2026-06-21 11:15:09 -0400

    Use PNG project banners

--- src/js/projectBanners.js
+++ src/js/projectBanners.js
@@ -1,22 +1,22 @@
 /* ── projectBanners.js ───────────────────────────────────── */
 const projectBanners = [
   {
-    image: "./src/img/itens-banner/banner-1.svg",
+    image: "./src/img/itens-banner/banner-1.png",
     alt: "Banner 1",
     link: "",
   },
   {
-    image: "./src/img/itens-banner/banner-2.svg",
+    image: "./src/img/itens-banner/banner-2.png",
     alt: "Banner 2",
     link: "",
   },
   {
-    image: "./src/img/itens-banner/banner-3.svg",
+    image: "./src/img/itens-banner/banner-3.png",
     alt: "Banner 3",
     link: "",
   },
   {
-    image: "./src/img/itens-banner/banner-4.svg",
+    image: "./src/img/itens-banner/banner-4.png",
     alt: "Banner 4",
     link: "",
   },
